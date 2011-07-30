e55c941f5a5a6c05d96dd8a8fea6a9052d1ff784
Jeff Hodges <jeff@somethingsimilar.com>
2011-07-30 04:40:42 -0700

    jquery version
    hacked out of the bones of http://www.webspeaks.in/2011/07/new-gmail-like-floating-toolbar-jquery.html but the real core logic is reverse engineered from gmail itself. check bar.js

---
+++ bar.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+  var $buttonBar = $('.button_bar');
+  var $aboveButtonBar = $('<div class="above_button_bar"></div>');
+  var originalWidth = $buttonBar.width();
+  // FIXME I know that originalOffset is supposed to use position, but should originalLeft?
+  var originalLeft = $buttonBar.position().left;
+  var originalOffset = $buttonBar.position().top;
+
+  // the width and left will change on you when you scroll unless you specify
+  // them back on in the style attribute. I'm not actually completely positive
+  // of that, but It's What Gmail Does. Sorry. :(
+  var scrolledStyle = 'position:fixed!important; top:0px; width: '+originalWidth.toString(10) +'px; '+originalLeft.toString(10)+'px;';
+
+  $(window).scroll(function(){
+    /* Get the current distance of the element from the top */
+    var offsetTop = $buttonBar.offset().top;
+    /* Get distance from the top of window through which we have scrolled */
+    var s = parseInt($(window).scrollTop(), 10);
+    var fixMe = true;
+
+    if(s > offsetTop){
+      // Somehow, inserting an item of the same height makes this work.
+      // I don't understand why. Others do not do this, and are forced to use
+      // position:absolute; and top in the defintion of .button_bar. By doing
+      // this, we do not require it.
+      $aboveButtonBar.insertBefore($buttonBar)
+      // For reasons unknown to me, this must be a style setting, not an
+      // addClass.
+      $buttonBar.attr('style', scrolledStyle)
+    }
+    if(s < parseInt(originalOffset, 10)){
+      $aboveButtonBar.remove()
+      $buttonBar.attr('style', '');
+    }
+  });
+});
